7efec1be16264d5e43490c7df89db276f6343133
Nicolás Narria <nnarria@nicolass-macbook-pro.local>
2016-04-11 11:59:19 -0300

    Initial Commit

---
+++ TareaReproductorMusica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TareaReproductorMusica
+//
+//  Created by Nicolás Narria on 4/11/16.
+//  Copyright © 2016 Nicolás Narria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
